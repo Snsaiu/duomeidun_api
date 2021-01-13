6e268cefa0b861d4fec33701f2d88479aff82096
snsaiu <snsaiu@outlook.com>
2021-01-13 14:17:54 +0800

    init

--- api-admin/src/main/java/com/lmxdawn/api/admin/controller/auth/LoginController.java
+++ api-admin/src/main/java/com/lmxdawn/api/admin/controller/auth/LoginController.java
@@ -18,10 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
